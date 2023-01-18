19c85e02dd18af7b2c0a7cf2339853a7614f5b33
Richard <richardnunez@derco.cl>
2023-01-18 19:07:45 -0300

    74. Open Graph Metatag

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -2,11 +2,14 @@ import { ReactNode } from 'react';
 import Head from 'next/head';
 import { Navbar } from '../ui/Navbar';
 
+const origin = (typeof window === 'undefined') ? '' : window.location.origin 
+
 export const Layout = (
   {children, title} : {
     children:ReactNode
     title?: string
   } ) => {
+
   return (
     <>
       <Head>
@@ -14,6 +17,9 @@ export const Layout = (
             <meta name='author' content='Rich'/>
             <meta name='description' content={`Información sobre el pokémon, ${title}`}/>
             <meta name='keywords' content={`${title}, pokemon, pokedex`}/>
+            <meta property="og:title" content={`Información sobre ${title}`} />
+            <meta property="og:description" content={`Esta es la página sobre ${title}`} />
+            <meta property="og:image" content={`${origin}/img/banner.png`} />
       </Head>
       <Navbar/>
       <main style={{
